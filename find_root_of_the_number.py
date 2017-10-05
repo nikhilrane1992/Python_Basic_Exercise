4674752f343f26c2decb9e7a9639519f08080f0c
Nikhil Rane <nikhilrane1992@gmail.com>
2017-10-05 16:49:55 +0530

    - find_root_of_the_number

---
+++ find_root_of_the_number.py
@@ -0,0 +1,9 @@
+"""
+Find out the square, cube, fourth and fifth root of the given number
+"""
+
+num = input("Enter Number: ")
+print "Square root of the number: {}".format(num ** (1 / 2.0))
+print "Cube root of the number: {}".format(num ** (1 / 3.0))
+print "Fourth root of the number: {}".format(num ** (1 / 3.0))
+print "Fifth root of the number: {}".format(num ** (1 / 3.0))
