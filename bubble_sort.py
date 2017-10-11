8a13858b7d0e33d974daacbe1e3bc711db4bf840
Nikhil Rane <nikhilrane1992@gmail.com>
2017-10-11 11:57:00 +0530

    Solve indentation

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,8 +3,8 @@
 input_list = [2,5,6,3,4,9,5,2,33,66,33,88,99,2,56]
 
 for i in range(len(input_list)):
-	for j in range(0, i):
-		if input_list[i] < input_list[j]:
-			input_list[i], input_list[j] = input_list[j], input_list[i]
+    for j in range(0, i):
+	if input_list[i] < input_list[j]:
+    	    input_list[i], input_list[j] = input_list[j], input_list[i]
 
 print input_list
